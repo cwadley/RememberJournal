558b6f9d30424546630fc006952f6280eba579c0
cwadley <clintwadley@gmail.com>
2016-03-15 21:40:54 -0400

    Added bootstrap files, created index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html>
+    <head>
+        <title>Remember Journal</title>
+
+        <!-- Link in Bootstrap, font-awesome, and custom stylesheets -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+    </head
+
+    <body>
+
+
+
+        <!-- Include jQuery and bootstrap js file-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
